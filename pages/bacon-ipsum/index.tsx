8be1ea903c8c65e3971be87e435ab09a79cd1d32
AristosAchaion <robbybyron@gmail.com>
2020-05-09 17:09:26 +0100

    initialized repo

---
+++ pages/bacon-ipsum/index.tsx
@@ -0,0 +1,20 @@
+import Link from'next/link';
+
+const BaconIpsum = () => (
+    <>
+        <p>
+            Bacon ipsum dolor amet corned beef doner capicola t-bone cow pork pig jowl.
+            Short ribs beef fatback, shoulder pork belly swine ham pork biltong tenderloin bacon jerky andouille.
+            Bacon sirloin chuck, pork loin pig filet mignon strip steak beef ribs biltong beef ham hock.
+            Short loin rump tenderloin spare ribs tri-tip, meatball meatloaf ham hock tail.
+            Meatball ham hock leberkas hamburger salami drumstick pork belly swine fatback ham tenderloin pig kevin.
+            Chicken landjaeger sausage, cow turducken swine capicola.
+            Tri-tip landjaeger biltong, kevin shoulder corned beef pig short ribs strip steak shank doner pork chop bresaola porchetta ham hock.
+        </p>
+        <Link href ='/'>
+            <button>Home</button>
+        </Link>
+    </>
+);
+
+export default BaconIpsum;
---
+++ pages/dolorem-ipsum/index.tsx
@@ -0,0 +1,23 @@
+import Link from'next/link';
+
+const DoloremIpsum = () => (
+    <>
+        <h1>Taken from Cicero's <i>De finibus bonorum et malorum</i></h1>
+        <h3>This is the text from which the text of Lorem Ipsum is generated, resulting in text which looks like Latin but is gibberish</h3>
+        <p>
+            [32] Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. 
+            Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit amet consectetur adipisci[ng]velit, sed quia non-numquameius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. 
+            Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipitlaboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui inea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur? 
+            [33] At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non-provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. 
+            Et harum quidem rerum facilis est et expedita distinctio. 
+            Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. 
+            Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non-recusandae. 
+            Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat
+        </p>
+        <Link href ='/'>
+            <button>Home</button>
+        </Link>
+    </>
+);
+
+export default DoloremIpsum;
